756149353572fb375d4abee981a7edac0a3cd772
Aditya Khattar <55162973+BugCrush@users.noreply.github.com>
2020-10-30 01:01:20 +0530

    Update funcop.cpp
    hacktoberfest
    hacktoberfest2020

--- funcop.cpp
+++ funcop.cpp
@@ -1,5 +1,7 @@
 // funcop.cpp by Bill Weinman <http://bw.org/>
 #include <iostream>
+#include<math>
+#define N 100
 using namespace std;
 
 int f( int i) {
@@ -8,8 +10,13 @@ int f( int i) {
 }
 
 int main( int argc, char ** argv ) {
-	int i;
+	int i,a,b;
 	i = f(47);
 	printf("i is %d\n", i);
+	
+	a=N/100;
+	b=N*100;
+	printf(a*b);
+	
 	return 0;
 }
